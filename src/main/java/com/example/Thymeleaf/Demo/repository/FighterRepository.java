6e4a86d1af91edfd9a68ae9c492bd2281b7a4ba1
Othniel Powell <othniel@live.ca>
2026-03-08 14:28:19 -0400

    added the first first derived query method

--- src/main/java/com/example/Thymeleaf/Demo/repository/FighterRepository.java
+++ src/main/java/com/example/Thymeleaf/Demo/repository/FighterRepository.java
@@ -3,8 +3,10 @@ package com.example.Thymeleaf.Demo.repository;
 import com.example.Thymeleaf.Demo.Model.Fighter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface FighterRepository extends JpaRepository<Fighter, Integer> {
-
+Page<Fighter> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
