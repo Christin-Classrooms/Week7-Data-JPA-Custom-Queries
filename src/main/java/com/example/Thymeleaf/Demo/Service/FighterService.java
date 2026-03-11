7110634ed418e68d79f31a526d7d4f23103f13cd
CL <leungc22@gmail.com>
2026-03-11 13:29:13 -0400

    Lab 4: Implement custom repository queries with pagination and sorting

--- src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
+++ src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
@@ -1,16 +1,40 @@
 package com.example.Thymeleaf.Demo.Service;
 
-import com.example.Thymeleaf.Demo.Model.Fighter;
-import com.example.Thymeleaf.Demo.repository.FighterRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.Thymeleaf.Demo.Model.Fighter;
+import com.example.Thymeleaf.Demo.repository.FighterRepository;
 
 @Service
 public class FighterService {
 
+
     private final FighterRepository fighterRepository;
+
+    public Page<Fighter> findAll(Pageable pageable) {
+        return fighterRepository.findAll(pageable);
+    }
+    public Page<Fighter> findByNameContainingIgnoreCase(String name, Pageable pageable) {
+        return fighterRepository.findByNameContainingIgnoreCase(name, pageable);
+    }
+    public Page<Fighter> findByHealthGreaterThan(int health, Pageable pageable) {
+        return fighterRepository.findByHealthGreaterThan(health, pageable);
+    }
+
+    public Page<Fighter> findByHealthGreaterThanEqual(int health, Pageable pageable) {
+        return fighterRepository.findByHealthGreaterThanEqual(health, pageable);
+    }
+    public Page<Fighter> findStrongestFighters(Pageable pageable) {
+        return fighterRepository.findStrongestFighters(pageable);
+    }
+    public Page<Fighter> findBalancedFighters(double minHealth, double maxDamage, Pageable pageable) {
+        return fighterRepository.findBalancedFighters(minHealth, maxDamage, pageable);
+    }
 
     public FighterService(FighterRepository fighterRepository) {
         this.fighterRepository = fighterRepository;
