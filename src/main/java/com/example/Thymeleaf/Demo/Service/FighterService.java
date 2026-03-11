fe00c9adf9d085f4a5ee6b6b690dc4860c4940bc
Tyler Ly <tylerly385@gmail.com>
2026-03-11 11:11:58 -0400

    Lab 4: Implement custom repository queries with pagination and sorting

--- src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
+++ src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
@@ -2,6 +2,8 @@ package com.example.Thymeleaf.Demo.Service;
 
 import com.example.Thymeleaf.Demo.Model.Fighter;
 import com.example.Thymeleaf.Demo.repository.FighterRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,22 +15,53 @@ public class FighterService {
     private final FighterRepository fighterRepository;
 
     public FighterService(FighterRepository fighterRepository) {
+
         this.fighterRepository = fighterRepository;
     }
 
     public List<Fighter> getAllFighters() {
+
         return fighterRepository.findAll();
     }
 
+    public Page<Fighter> getAllFightersPageable(Pageable pageable) {
+
+        return fighterRepository.findAll(pageable);
+
+    }
+
     public void addFighter(Fighter fighter) {
+
         fighterRepository.save(fighter);
     }
 
     public Optional<Fighter> getFighterById(int id) {
+
         return fighterRepository.findById(id);
     }
 
+    public Page<Fighter> getFighterByIdPageable(String name, Pageable pageable) {
+        return fighterRepository.findByNameContainingIgnoreCase(name, pageable);
+    }
+
+    public Page<Fighter> findByNameContainingIgnoreCase(String name, Pageable pageable) {
+        return fighterRepository.findByNameContainingIgnoreCase(name, pageable);
+    }
+
+    public Page<Fighter> findByHealthGreaterThan(int health, Pageable pageable) {
+        return fighterRepository.findByHealthGreaterThan(health, pageable);
+    }
+
+    public Page<Fighter> findStrongestFighters(Pageable pageable) {
+        return fighterRepository.findStrongestFighters(pageable);
+    }
+
+    public Page<Fighter> findBalancedFighters(double minHealth, double maxDamage,  Pageable pageable) {
+        return fighterRepository.findBalancedFighters(minHealth, maxDamage, pageable);
+    }
+
     public void deleteFighter(int id) {
+
         fighterRepository.deleteById(id);
     }
 
