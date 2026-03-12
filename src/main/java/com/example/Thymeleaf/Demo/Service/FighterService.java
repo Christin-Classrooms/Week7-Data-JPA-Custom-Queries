956e60571cf2c0fb60fe54e39136b5961fba9661
Aviish11 <avish.auckloo1108@gmail.com>
2026-03-11 22:26:40 -0400

    lab 4 complete

--- src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
+++ src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
@@ -2,6 +2,9 @@ package com.example.Thymeleaf.Demo.Service;
 
 import com.example.Thymeleaf.Demo.Model.Fighter;
 import com.example.Thymeleaf.Demo.repository.FighterRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,4 +43,27 @@ public class FighterService {
         return fighterRepository.count();
     }
 
-}
+    public Page<Fighter> findByName(String search, Pageable pageable) {
+        return fighterRepository.findByNameContainingIgnoreCase(search, pageable);
+        
+    }
+
+    public Page<Fighter> findByHealth(int i, Pageable pageable) {
+        return fighterRepository.findByHealthGreaterThan(i, pageable);
+    }
+
+    public Page<Fighter> findStrongest(Pageable pageable) {
+        return fighterRepository.findStrongestFighters(pageable);
+    }
+
+    public Page<Fighter> findBalanced(int i, int j, Pageable pageable) {
+        return fighterRepository.findBalancedFighters(i, j, pageable);
+    }
+
+    public Page<Fighter> getAllFighters(Pageable pageable) {
+        return fighterRepository.findAll(pageable);
+    }
+    }
+
+    
+
--- src/main/java/com/example/Thymeleaf/Demo/controllers/FighterController.java
+++ src/main/java/com/example/Thymeleaf/Demo/controllers/FighterController.java
@@ -2,9 +2,16 @@ package com.example.Thymeleaf.Demo.controllers;
 
 import com.example.Thymeleaf.Demo.Model.Fighter;
 import com.example.Thymeleaf.Demo.Service.FighterService;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,12 +24,60 @@ public class FighterController {
         this.fighterService = fighterService;
     }
 
+   
     @GetMapping("/fighters")
-    public String getFighters(Model model) {
-        List<Fighter> fighters = fighterService.getAllFighters();
+    public String getFighters(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sort,
+            @RequestParam(defaultValue = "ASC") String direction,
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) String filterType,
+            Model model
+    ) {
 
-        model.addAttribute("fighters", fighters);
-        model.addAttribute("total", fighters.size());
+        Sort sorting = direction.equalsIgnoreCase("ASC") ?
+                Sort.by(sort).ascending() :
+                Sort.by(sort).descending();
+
+        Pageable pageable = PageRequest.of(page, size, sorting);
+
+        Page<Fighter> fighterPage;
+
+        if ("name".equals(filterType) && search != null) {
+            fighterPage = fighterService.findByName(search, pageable);
+        }
+
+        else if ("health".equals(filterType)) {
+            fighterPage = fighterService.findByHealth(1200, pageable);
+        }
+
+        else if ("strongest".equals(filterType)) {
+            fighterPage = fighterService.findStrongest(pageable);
+        }
+
+        else if ("balanced".equals(filterType)) {
+            fighterPage = fighterService.findBalanced(1100, 80, pageable);
+        }
+
+        else {
+            fighterPage = fighterService.getAllFighters(pageable);
+        }
+
+        model.addAttribute("fighters", fighterPage.getContent());
+        model.addAttribute("totalPages", fighterPage.getTotalPages());
+        model.addAttribute("totalElements", fighterPage.getTotalElements());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("pageSize", size);
+
+        model.addAttribute("hasPrevious", fighterPage.hasPrevious());
+        model.addAttribute("hasNext", fighterPage.hasNext());
+
+        model.addAttribute("search", search);
+        model.addAttribute("sort", sort);
+        model.addAttribute("direction", direction);
+        model.addAttribute("filterType", filterType);
+
         return "Fighters";
     }
 
